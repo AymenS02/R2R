c9719d2f9dc2e355058c15b9ccdb06bd7e0a92cd
Aymen Shoteri <aymenshoteri@gmail.com>
2025-01-02 08:11:53 -0500

    adding images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 z-999 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light">
       <div className="flex justify-between items-center w-full px-4 py-2">
-        <img src='public/logo.png' className="w-10" alt="Logo" />
+        <img src='./logo.png' className="w-10 hidden sm:block" alt="Logo" />
         <ul className="flex space-x-4">
           <li><Link to="/" className="hover:underline">Home</Link></li>
           <li><Link to="/about" className="hover:underline">About</Link></li>
