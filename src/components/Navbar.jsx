a835fbcd470aae6c2b7eb7d7f6a5ce6eb53dfdab
Aymen Shoteri <aymenshoteri@gmail.com>
2025-01-04 17:58:40 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="font-merriweather fixed top-0 left-0 z-20 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light">
       <div className="flex justify-between items-center w-full px-4 py-2 text-sm md:text-lg">
         <img src='./logo.png' className="w-10 hidden sm:block" alt="Logo" />
-        <ul className="flex space-x-4">
+        <ul className="flex md:space-x-4 space-x-2">
           <li><Link to="/" className="hover:underline">Home</Link></li>
           <li><Link to="/about" className="hover:underline">About</Link></li>
           <li><Link to="/contact" className="hover:underline">Contact</Link></li>
